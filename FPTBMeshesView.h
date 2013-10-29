9c3e3ae4aae176f1d71f28112af11e1ecbeb19d3
David Garcia Juan <david.garcia@hcuge.ch>
2013-10-29 14:48:35 +0100

    Adding FPTBMeshView

---
+++ FPTBMeshesView.h
@@ -0,0 +1,20 @@
+//
+//  FPTBMeshesView.h
+//  FromPolyToBrush
+//
+//  Created by David García Juan on 29/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "OsiriXAPI/vtkView.h"
+
+@interface FPTBMeshesView : VTKView
+{
+
+    
+    
+}
+
+@end
---
+++ FPTBMeshesView.m
@@ -0,0 +1,13 @@
+//
+//  FPTBMeshesView.m
+//  FromPolyToBrush
+//
+//  Created by David García Juan on 29/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "FPTBMeshesView.h"
+
+@implementation FPTBMeshesView
+
+@end
